3f76456798c79df87de2762e1631d67335aaf116
jacknoahcaldwell <jacknoahcaldwell@gmail.com>
2020-07-30 05:17:04 -0700

    Revert "kill it"
    This reverts commit aafe2e1eb289739ac9d33377f47ff131e8b3079e.

---
+++ public/scroll2.js
@@ -0,0 +1,35 @@
+gsap.set(".sticky", {yPercent: -100});
+let main = document.querySelector(".main-container");
+
+ScrollTrigger.create({
+  trigger: ".sticky",
+  start: "bottom bottom",
+  endTrigger: "footer",
+  end: "top bottom",
+  pin: true,
+  onRefreshInit: self => main.style.paddingBottom = self.trigger.offsetHeight + "px",
+  pinSpacing: false
+});
+
+gsap.to(".pContent", {
+  yPercent: -0,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".pContent",
+    start: "top ", 
+    // end: "bottom top",
+    scrub: true
+  }, 
+});
+
+gsap.to(".pImage", {
+  yPercent: 50,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".pContent",
+    start: "top", 
+    end: "bottom top",
+    scrub: true
+  }, 
+});
+gsap.to(".pContent2", {
